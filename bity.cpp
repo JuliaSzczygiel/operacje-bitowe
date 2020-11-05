3d1086aaeedd885c77c8deda1953cef2750bbadf
JuliaSzczygiel <juliaszczygiel.kontakt@gmail.com>
2020-11-05 11:00:45 +0100

    first commit

---
+++ bity.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <bitset>
+
+using namespace std;
+int main() { 
+int a, b;
+cout << "podaj a" << endl;
+cin >> a;
+cout << "podaj b" << endl;
+cin >> b;
+
+cout << bitset<8>(a|b).to_string();
+cout << bitset<8>(a^b).to_string();
+cout << bitset<8>(a&b).to_string();
+cout << bitset<8>(a<<=b).to_string();
+cout << bitset<8>(a>>=b).to_string();
+return 0;
+}
